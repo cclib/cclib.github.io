9dc7e151b481a01a78903c29864d255dddb7fd24
Karol M. Langner <karol.langner@gmail.com>
2015-05-25 00:49:29 -0400

    Update copyright years

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -41,7 +41,7 @@ master_doc = 'contents'
 
 # General information about the project.
 project = u'cclib'
-copyright = u'2014, cclib Development Team'
+copyright = u'2014-2015, cclib Development Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
