071df513a2bc1264480b08e8f1f918f9f19641ea
Eric Berquist <eric.berquist@gmail.com>
2018-06-28 22:21:47 -0400

    Use functions again in attributes.py and coverage.py

--- sphinx/attributes.py
+++ sphinx/attributes.py
@@ -7,62 +7,71 @@ from docs_common import check_cclib
 import cclib
 check_cclib(cclib)
 
-# Need to parse the ccData docstring, since only that currently
-# contains all the information needed for this table.
-data_doc = cclib.parser.data.ccData.__doc__
-attributes = [line for line in data_doc.split('\n') if line[:8].strip() == '']
-attributes = [line for line in attributes if "--" in line]
 
-# These are the widths of the columns in the table
-wattr = 20
-wdesc = 65
-wunit = 28
-wtype = 32
+def generate_attributes():
+    lines = []
 
-dashes = "    "
-for w in [wattr, wdesc, wunit, wtype]:
-    dashes += "="*(w-1) + " "
-header = "    "
-header += "Name".ljust(wattr)
-header += "Description".ljust(wdesc)
-header += "Units".ljust(wunit)
-header += "Data type".ljust(wtype)
-print(dashes)
-print(header)
-print(dashes)
+    # Need to parse the ccData docstring, since only that currently
+    # contains all the information needed for this table.
+    data_doc = cclib.parser.data.ccData.__doc__
+    attributes = [line for line in data_doc.split('\n') if line[:8].strip() == '']
+    attributes = [line for line in attributes if "--" in line]
 
-names = []
-for line in attributes:
+    # These are the widths of the columns in the table
+    wattr = 20
+    wdesc = 65
+    wunit = 28
+    wtype = 32
 
-    # There is always a double dash after the name.
-    attr, desc = line.strip().split(' -- ')
-    names.append(attr)
+    dashes = "    "
+    for w in [wattr, wdesc, wunit, wtype]:
+        dashes += "="*(w-1) + " "
+    header = "    "
+    header += "Name".ljust(wattr)
+    header += "Description".ljust(wdesc)
+    header += "Units".ljust(wunit)
+    header += "Data type".ljust(wtype)
+    lines.append(dashes)
+    lines.append(header)
+    lines.append(dashes)
 
-    # The type and unit are in parentheses, but these
-    # are not always the only parentheses on the line.
-    other = desc.split('(')[-1]
-    desc = desc[:-len(other)-1].strip()
-    other = other.split(')')[0]
+    names = []
+    for line in attributes:
 
-    # Furthermore, the unit is not always there.
-    if "," in other:
-        atype, aunit = other.split(", ")
-    else:
-        atype = other
-        aunit = ''
+        # There is always a double dash after the name.
+        attr, desc = line.strip().split(' -- ')
+        names.append(attr)
 
-    # Print the line with columns align to the table. Note that
-    # the description sometimes contain Unicode characters, so
-    # decode-encode when justifying to get the correct length.
-    attr = ("`%s`_" % attr).ljust(wattr)
-    desc = desc.ljust(wdesc)
-    aunit = aunit.ljust(wunit)
-    for i in range(1,4):
-        atype = atype.replace('[%i]' % i, ' of rank %i' % i)
-    print("    " + attr + desc + aunit + atype)
+        # The type and unit are in parentheses, but these
+        # are not always the only parentheses on the line.
+        other = desc.split('(')[-1]
+        desc = desc[:-len(other)-1].strip()
+        other = other.split(')')[0]
 
-print(dashes)
-print("")
+        # Furthermore, the unit is not always there.
+        if "," in other:
+            atype, aunit = other.split(", ")
+        else:
+            atype = other
+            aunit = ''
 
-for n in names:
-    print(".. _`%s`: data_notes.html#%s" % (n, n))
+        # Print the line with columns align to the table. Note that
+        # the description sometimes contain Unicode characters, so
+        # decode-encode when justifying to get the correct length.
+        attr = ("`%s`_" % attr).ljust(wattr)
+        desc = desc.ljust(wdesc)
+        aunit = aunit.ljust(wunit)
+        for i in range(1,4):
+            atype = atype.replace('[%i]' % i, ' of rank %i' % i)
+        lines.append("    " + attr + desc + aunit + atype)
+
+    lines.append(dashes)
+    lines.append("")
+
+    for n in names:
+        lines.append(".. _`%s`: data_notes.html#%s" % (n, n))
+
+    return "\n".join(lines)
+
+if __name__ == "__main__":
+    print(generate_attributes())
--- sphinx/coverage.py
+++ sphinx/coverage.py
@@ -11,91 +11,100 @@ from docs_common import check_cclib
 import cclib
 check_cclib(cclib)
 
-# Change directory to where tests are and add it to the path. Because there are
-# separate directories for different branches/versions, and we use a symlink to
-# point to the one we want, we need to test the real path this link resolves to.
-if "cclib_prod" in os.path.realpath('cclib'):
-    testpath = "_build/cclib_prod"
-else:
-    assert "cclib_dev" in os.path.realpath('cclib')
-    testpath = "_build/cclib_dev"
 
-os.chdir(testpath)
+def generate_coverage():
+    lines = []
 
-thispath = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(1, thispath)
+    # Change directory to where tests are and add it to the path. Because there are
+    # separate directories for different branches/versions, and we use a symlink to
+    # point to the one we want, we need to test the real path this link resolves to.
+    if "cclib_prod" in os.path.realpath('cclib'):
+        testpath = "_build/cclib_prod"
+    else:
+        assert "cclib_dev" in os.path.realpath('cclib')
+        testpath = "_build/cclib_dev"
 
-from test.test_data import (all_modules, all_parsers, parser_names, DataSuite)
-import inspect
-ds_args = inspect.getargspec(DataSuite.__init__).args
-logpath = thispath + "/coverage.tests.log"
-try:
-    with open(logpath, "w") as flog:
-        stdout_backup = sys.stdout
-        sys.stdout = flog
-        alltests = {}
-        for p in parser_names:
-            assert 'parsers' in ds_args
-            suite = DataSuite(parsers={p: all_parsers[p]}, modules=all_modules, stream=flog)
-            suite.testall()
-            alltests[p] = [{'data': t.data} for t in suite.alltests]
-        sys.stdout = stdout_backup
-except Exception as e:
-    print("Unit tests did not run correctly. Check log file for errors:")
-    with open(logpath) as fh:
-        print(fh.read())
-    print(e)
-    sys.exit(1)
+    os.chdir(testpath)
 
-ncols = len(parser_names) + 1
-colwidth = 20
-colfmt = "%%-%is" % colwidth
-dashes = ("=" * (colwidth - 1) + " ") * ncols
+    thispath = os.path.dirname(os.path.realpath(__file__))
+    sys.path.insert(1, thispath)
 
-print(dashes)
-print(colfmt * ncols % tuple(["attributes"] + parser_names))
-print(dashes)
+    from test.test_data import (all_modules, all_parsers, parser_names, DataSuite)
+    import inspect
+    ds_args = inspect.getargspec(DataSuite.__init__).args
+    logpath = thispath + "/coverage.tests.log"
+    try:
+        with open(logpath, "w") as flog:
+            stdout_backup = sys.stdout
+            sys.stdout = flog
+            alltests = {}
+            for p in parser_names:
+                assert 'parsers' in ds_args
+                suite = DataSuite(parsers={p: all_parsers[p]}, modules=all_modules, stream=flog)
+                suite.testall()
+                alltests[p] = [{'data': t.data} for t in suite.alltests]
+            sys.stdout = stdout_backup
+    except Exception as e:
+        print("Unit tests did not run correctly. Check log file for errors:")
+        with open(logpath) as fh:
+            print(fh.read())
+        print(e)
+        sys.exit(1)
 
-# Eventually we want to move this to cclib, too.
-not_applicable = {
-    'ADF' : ['aonames', 'ccenergies', 'mpenergies'],
-    'DALTON' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'GAMESS' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'GAMESSUK' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'Gaussian' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'Jaguar' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'Molpro' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'NWChem' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'ORCA' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'Psi' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-    'QChem' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
-}
-not_possible = {
-    'Psi' : ['aooverlaps', 'vibirs'],
-    'QChem' : ['aooverlaps'],
-}
+    ncols = len(parser_names) + 1
+    colwidth = 20
+    colfmt = "%%-%is" % colwidth
+    dashes = ("=" * (colwidth - 1) + " ") * ncols
 
-# For each attribute, get a list of Boolean values for each parser that flags
-# if it has been parsed by at least one unit test. Substitute an OK sign or
-# T/D appropriately, with the exception of attributes that have been explicitely
-# designated as N/A.
-attributes = sorted(cclib.parser.data.ccData._attrlist)
-for attr in attributes:
-    parsed = [any([attr in t['data'].__dict__ for t in alltests[p]]) for p in parser_names]
-    for ip, p in enumerate(parsed):
-        if p:
-            parsed[ip] = "√"
-        else:
-            if attr in not_applicable.get(parser_names[ip], []):
-                parsed[ip] = "N/A"
-            elif attr in not_possible.get(parser_names[ip], []):
-                parsed[ip] = "N/P"
+    lines.append(dashes)
+    lines.append(colfmt * ncols % tuple(["attributes"] + parser_names))
+    lines.append(dashes)
+
+    # Eventually we want to move this to cclib, too.
+    not_applicable = {
+        'ADF' : ['aonames', 'ccenergies', 'mpenergies'],
+        'DALTON' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'GAMESS' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'GAMESSUK' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'Gaussian' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'Jaguar' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'Molpro' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'NWChem' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'ORCA' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'Psi' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+        'QChem' : ['fonames', 'fooverlaps', 'fragnames', 'frags'],
+    }
+    not_possible = {
+        'Psi' : ['aooverlaps', 'vibirs'],
+        'QChem' : ['aooverlaps'],
+    }
+
+    # For each attribute, get a list of Boolean values for each parser that flags
+    # if it has been parsed by at least one unit test. Substitute an OK sign or
+    # T/D appropriately, with the exception of attributes that have been explicitely
+    # designated as N/A.
+    attributes = sorted(cclib.parser.data.ccData._attrlist)
+    for attr in attributes:
+        parsed = [any([attr in t['data'].__dict__ for t in alltests[p]]) for p in parser_names]
+        for ip, p in enumerate(parsed):
+            if p:
+                parsed[ip] = "√"
             else:
-                parsed[ip] = "T/D"
-    print(colfmt*ncols % tuple(["`%s`_" % attr] + parsed))
+                if attr in not_applicable.get(parser_names[ip], []):
+                    parsed[ip] = "N/A"
+                elif attr in not_possible.get(parser_names[ip], []):
+                    parsed[ip] = "N/P"
+                else:
+                    parsed[ip] = "T/D"
+        lines.append(colfmt*ncols % tuple(["`%s`_" % attr] + parsed))
 
-print(dashes)
-print("")
+    lines.append(dashes)
+    lines.append("")
 
-for attr in attributes:
-    print(".. _`%s`: data_notes.html#%s" % (attr, attr))
+    for attr in attributes:
+        lines.append(".. _`%s`: data_notes.html#%s" % (attr, attr))
+
+    return "\n".join(lines)
+
+if __name__ == "__main__":
+    print(generate_coverage())
